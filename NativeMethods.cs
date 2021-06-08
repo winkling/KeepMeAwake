546e69d9c79d490071a66faa9e5757a6b6d613ce
Kun Wang <mailwangkun@gmail.com>
2021-06-08 15:45:40 -0700

    Initial commit

---
+++ NativeMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace KeepMeAwake
+{
+    public static class NativeMethods
+    {
+        [DllImport("Kernel32.dll")]
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+    }
+
+    public enum EXECUTION_STATE : uint
+    {
+        ES_AWAYMODE_REQUIRED = 0x00000040,
+        ES_CONTINUOUS = 0x80000000,
+        ES_DISPLAY_REQUIRED = 0x00000002,
+        ES_SYSTEM_REQUIRED = 0x00000001,
+        ES_USER_PRESENT = 0x00000004
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace KeepMeAwake
+{
+    class Program
+    {
+        static ManualResetEvent mStopFlag = new ManualResetEvent(false);
+        static int mInterval = 30 * 1000; //30s
+
+        static void Main(string[] args)
+        {
+            Log("Keep me awake started.");
+
+            Thread t = new Thread(new ThreadStart(Worker));
+            t.Start();
+
+            Log("Press any key to exit.");
+            Console.ReadKey();
+
+            mStopFlag.Set();
+            Log("Received terminate signal.");
+        }
+
+        private static void Worker()
+        {
+            for (; ; )
+            {
+                var stat = NativeMethods.SetThreadExecutionState(EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+                Log("Sent ES_SYSTEM_REQUIRED.");
+
+                if (mStopFlag.WaitOne(mInterval))
+                    break;
+            }
+        }
+
+        static void Log(string format, params object?[] args)
+        {
+            Console.Write("{0}> ", DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss"));
+            Console.WriteLine(format, args);
+        }
+    }
+}
